e2ac057a096044025ee256c65b64d6f33ead3c16
danielmartellini <danielbmartellini@gmail.com>
2021-05-11 02:57:45 +0100

    more design and new js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //this is the function that hides the password checker, and shows it again after in case it's been hidden
 var a;
 function showHide() {
-  if (a == 1) {
+  if ( a == 1) {
     document.getElementById("passChecker").style.display = "flex";
     return (a = 0);
   } else {
@@ -10,79 +10,109 @@ function showHide() {
   }
 }
 
-function _id(name) {
+function idName(name) {
   return document.getElementById(name);
 }
 
-function _class(name) {
+function className(name) {
   return document.getElementsByClassName(name);
 }
 
-_class("togglePassword")[0].addEventListener("click", function () {
-  _class("togglePassword")[0].classList.toggle("active");
-  if (_id("passwordSpace").getAttribute("type") == "password") {
-    _id("passwordSpace").setAttribute("type", "text");
+className("togglePassword")[0].addEventListener("click", function () {
+  className("togglePassword")[0].classList.toggle("active");
+  if (idName("passwordSpace").getAttribute("type") == "password") {
+    idName("passwordSpace").setAttribute("type", "text");
   } else {
-    _id("passwordSpace").setAttribute("type", "password");
+    idName("passwordSpace").setAttribute("type", "password");
   }
 });
 
-_id("passwordSpace").addEventListener("focus", function () {
-  _class("passPolicies")[0].classList.add("active");
+idName("passwordSpace").addEventListener("focus", function () {
+  className("passPolicies")[0].classList.add("active");
 });
-_id("passwordSpace").addEventListener("blur", function () {
-  _class("passPolicies")[0].classList.remove("active");
+idName("passwordSpace").addEventListener("blur", function () {
+  className("passPolicies")[0].classList.remove("active");
 });
 
-_id("passwordSpace").addEventListener("keyup", function () {
-  let password = _id("passwordSpace").value;
+idName("passwordSpace").addEventListener("keyup", function () {
+  let password = idName("passwordSpace").value;
 
   if (/[A-Z]/.test(password)) {
-    _class("polUppercase")[0].classList.add("active");
+    className("polUppercase")[0].classList.add("active");
   } else {
-    _class("polUppercase")[0].classList.remove("active");
+    className("polUppercase")[0].classList.remove("active");
   }
   if (/[a-z]/.test(password)) {
-    _class("polLowercase")[0].classList.add("active");
+    className("polLowercase")[0].classList.add("active");
   } else {
-    _class("polLowercase")[0].classList.remove("active");
+    className("polLowercase")[0].classList.remove("active");
   }
 
   if (/[0-9]/.test(password)) {
-    _class("polNumber")[0].classList.add("active");
+    className("polNumber")[0].classList.add("active");
   } else {
-    _class("polNumber")[0].classList.remove("active");
+    className("polNumber")[0].classList.remove("active");
   }
 
   if (/[^A-Za-z0-9]/.test(password)) {
-    _class("polSpecial")[0].classList.add("active");
+    className("polSpecial")[0].classList.add("active");
   } else {
-    _class("polSpecial")[0].classList.remove("active");
+    className("polSpecial")[0].classList.remove("active");
   }
 
   if (password.length > 7) {
-    _class("polLength")[0].classList.add("active");
+    className("polLength")[0].classList.add("active");
   } else {
-    _class("polLength")[0].classList.remove("active");
+    className("polLength")[0].classList.remove("active");
   }
 });
 
-function mealTotal() {
-  var total;
-  let starter = +document.getElementById("starterDishes").value;
-  let mains = +document.getElementById("mainDishes").value;
-  let desserts = +document.getElementById("dessertDishes").value;
-  total = starter + mains + desserts;
-  console.log(total);
-  document.getElementById("total").innerHTML = total + " EURO";
+var total1=0;
+var total2=0;
+var total3=0;
+var total=0;
+
+function mealTotal1() {
+  let starter = +document.getElementById("starterDishes1").value;
+  let mains = +document.getElementById("mainDishes1").value;
+  let desserts = +document.getElementById("dessertDishes1").value;
+  total1 = starter + mains + desserts;
+  document.getElementById("total1").innerHTML =""+ total1 + " EURO";
+}
+function mealTotal2() {
+  let starter = +document.getElementById("starterDishes2").value;
+  let mains = +document.getElementById("mainDishes2").value;
+  let desserts = +document.getElementById("dessertDishes2").value;
+  total2 = starter + mains + desserts;
+  document.getElementById("total2").innerHTML ="    "+ total2 + " EURO";
+}
+function mealTotal3() {
+  let starter = +document.getElementById("starterDishes3").value;
+  let mains = +document.getElementById("mainDishes3").value;
+  let desserts = +document.getElementById("dessertDishes3").value;
+  total3 = starter + mains + desserts;
+  document.getElementById("total3").innerHTML ="    "+ total3 + " EURO";
 }
 
-document.getElementById("submit").addEventListener("click", mealTotal);
+console.log(total1+total2+total3);
 
+function totalFinal(){
+  var total;
+  total=total1+total2+total3;
+  document.getElementById("totalFinal").innerHTML = total;
+}
+
+
+document.getElementById("form1").addEventListener("input", mealTotal1);
+document.getElementById("form2").addEventListener("input", mealTotal2);
+document.getElementById("form3").addEventListener("input", mealTotal3);
+document.getElementById("menu").addEventListener("change", totalFinal);
+
+//function that fetches the API and brings it to the webpage
 document.getElementById("myBtn").addEventListener("click", getData);
 
 function getData() {
-  // console.log('test');
+
 
   //Get API
   fetch("https://randomuser.me/api/?results=5")
@@ -90,10 +120,10 @@ function getData() {
     .then((data) => {
       let author = data.results;
 
-      //Get Data Value
+      //declared a variable, to place the generated users
       let output = "";
 
-      //Get Data Loop Through
+      //loops the returned data for each user
       author.forEach(function (lists) {
         output += `
                 <div class="container">
@@ -112,7 +142,7 @@ function getData() {
                 </div> `;
       });
 
-      //Show On Our Screen All Data
+      //writes the user generator on my div with the id output
       document.getElementById("output").innerHTML = output;
     });
 }
