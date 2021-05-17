4b403864c69d58cdf8f0c144129ef5374b7eb89d
danielmartellini <danielbmartellini@gmail.com>
2021-05-17 21:44:55 +0100

    nearly done

--- js/scriptMenu.js
+++ js/scriptMenu.js
@@ -1,3 +1,4 @@
+//this page is not being used in my website, I just left it here to compare it with the one using jQuery
 function idName(name) {
   return document.getElementById(name);
 }
@@ -6,31 +7,44 @@ function className(name) {
   return document.getElementsByClassName(name);
 }
 
-
-var a=0;
-function showHide(parameter){
-  
-if ( a == 1) {
-  idName(parameter).style.display = "none";
-  return (a = 0);
-} else {
-  idName(parameter).style.display = "flex";
-  return (a = 1);
-}
+var a = 0;
+function showHide(parameter) {
+  if (a == 1) {
+    idName(parameter).style.display = "none";
+    return (a = 0);
+  } else {
+    idName(parameter).style.display = "flex";
+    return (a = 1);
+  }
 }
 
+idName("startersBtn").addEventListener(
+  "click",
+  function () {
+    showHide("myStarters");
+  },
+  false
+);
+idName("mainsBtn").addEventListener(
+  "click",
+  function () {
+    showHide("myMains");
+  },
+  false
+);
 
-idName("startersBtn").addEventListener("click",function(){
-  showHide("myStarters");
-}, false);
-idName("mainsBtn").addEventListener("click",function(){
-  showHide("myMains");
-}, false);
+idName("dessertsBtn").addEventListener(
+  "click",
+  function () {
+    showHide("myDesserts");
+  },
+  false
+);
 
-idName("dessertsBtn").addEventListener("click",function(){
-  showHide("myDesserts");
-}, false);
-
-idName("drinksBtn").addEventListener("click",function(){
-  showHide("myDrinks");
-}, false);
+idName("drinksBtn").addEventListener(
+  "click",
+  function () {
+    showHide("myDrinks");
+  },
+  false
+);
--- js/scriptMenu1.js
+++ js/scriptMenu1.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $("#startersBtn").click(function () {
     $("#myStarters").slideToggle(1000);
   });
-
+  //those functions will change the state of my display from null to flex with a slide effect
   $("#mainsBtn").click(function () {
     $("#myMains").slideToggle(1000);
   });
@@ -12,48 +12,52 @@ $(document).ready(function () {
   $("#drinksBtn").click(function () {
     $("#myDrinks").slideToggle(1000);
   });
-  $(".moveAround").click(function () {
+  //this will make my pizza move up and down
+  $(".moveAround").hover(function () {
     var box = $(this);
     box.animate({
       top: 100,
     });
     box.animate({
-      top: 400,
+      top: 500,
     });
     box.animate({
       top: 100,
     });
   });
+  //will creat an alert when the pizza is clicked
+  //will also make the pizza disappear once it's clicked
+  $(".moveAround").click(function () {
+    $(this).hide();
+    alert(
+      "You just got 15% off your next visit to our Restaurant, just mention the Website's flying Pizza to your server* *Rules Apply"
+    );
+  });
+  //creating a variable to store the Jquery sintax
 
-  /*  
-  $("#pic").on({
-    mouseenter: function () {
-      $(this).css("background-image", "url(../img/background2.jpg)");
-    },
-  });*/
   var picture = $("#pic");
 
   var backgrounds = new Array(
     "url(../img/background.jpg)",
     "url(../img/background2.jpg)",
-    "url(../img/background3.jpg)"
+    "url(../img/background3.jpg)",
+    "url(../img/background4.jpg)",
+    "url(../img/background5.jpg)"
   );
   //setting a counter
   var currentPic = 0;
 
   function nextPicture() {
-    picture
-      .css(
-        "background-image",
-        //when it reaches the last img of the array it goes back to the first one, so it's essentially a loop
-        backgrounds[(currentPic = ++currentPic % backgrounds.length)]
-      )
-      .animate({ opacity: 1 }, { duration: 1000 });
+    picture.css(
+      "background-image",
+      //when it reaches the last img of the array it goes back to the first one, so it's essentially a loop
+      backgrounds[(currentPic = ++currentPic % backgrounds.length)]
+    );
 
-    setTimeout(nextPicture, 2000);
+    setTimeout(nextPicture, 3000);
   }
   //Here I'm calling the function nextPicure
-  setTimeout(nextPicture, 2000);
+  setTimeout(nextPicture, 3000);
   picture.css("background-image", backgrounds[0]);
 
   $(".menuCard")
