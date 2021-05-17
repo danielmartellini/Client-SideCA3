39466ff1e7766575fd6b16f60cf4bf8f48def944
danielmartellini <danielbmartellini@gmail.com>
2021-05-17 19:39:40 +0100

    more animations

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//I've created this function so I can access my classes typing less, instead of document.getElementById(name) I can use idName(name) 
+//I've created this function so I can access my classes typing less, instead of document.getElementById(name) I can use idName(name)
 function idName(name) {
   return document.getElementById(name);
 }
@@ -7,9 +7,19 @@ function className(name) {
   return document.getElementsByClassName(name);
 }
 
+$(document).ready(function () {
+  //this function will be able to Hide my password Checker
+  $("#clickHide").click(function () {
+    $("#passChecker").slideToggle(1000);
+  });
+
+  $("#myBtn").click(function () {
+    $(this).html("Generate New Guests");
+  });
+});
 
 //this is the function that hides the password checker, and shows it again after in case it's been hidden
-var a;
+/*var a;
 function showHide(parameter){
   if ( a == 1) {
     idName(parameter).style.display = "flex";
@@ -23,7 +33,7 @@ function showHide(parameter){
 idName("clickHide").addEventListener("click",function(){
   showHide("passChecker");
 }, false);
-
+*/
 
 //here I'm adding a class to my div, so I can style/change back, only clicking the button
 
@@ -76,81 +86,175 @@ idName("passwordSpace").addEventListener("keyup", function () {
   }
 });
 //I declared the variables outside so they would be global
-var total1=0;
-var meal1=idName("startersTotal");
+var total1 = 0;
+var meal1 = idName("startersTotal");
 
-var total=0;
+var total = 0;
 
 //function to add-up a meal total
 
-function mealTotal(param1,param2,param3,param4,totalDisplay) {
+function mealTotal(param1, param2, param3, param4, totalDisplay) {
   let starter = +idName(param1).value;
   let mains = +idName(param2).value;
   let drinks = +idName(param3).value;
   let desserts = +idName(param4).value;
-  total1 = starter + mains + desserts+drinks;
-  idName(totalDisplay).value=total1
-  idName(totalDisplay).innerHTML =total1 + " EURO";}
+  total1 = starter + mains + desserts + drinks;
+  idName(totalDisplay).value = total1;
+  idName(totalDisplay).innerHTML = total1 + " EURO";
+}
 
-  idName("form1").addEventListener("input",function(){
-    mealTotal("starterDishes1","mainDishes1","dessertDishes1","drinks1","total1");
-  }, false);
-  idName("form2").addEventListener("input",function(){
-    mealTotal("starterDishes2","mainDishes2","dessertDishes2","drinks2","total2");
-  }, false);
-  idName("form3").addEventListener("input",function(){
-    mealTotal("starterDishes3","mainDishes3","dessertDishes3","drinks3","total3");
-  }, false);
-  idName("form4").addEventListener("input",function(){
-    mealTotal("starterDishes4","mainDishes4","dessertDishes4","drinks4","total4");
-  }, false);
-  idName("form5").addEventListener("input",function(){
-    mealTotal("starterDishes5","mainDishes5","dessertDishes5","drinks5","total5");
-  }, false);
+idName("form1").addEventListener(
+  "input",
+  function () {
+    mealTotal(
+      "starterDishes1",
+      "mainDishes1",
+      "dessertDishes1",
+      "drinks1",
+      "total1"
+    );
+  },
+  false
+);
+idName("form2").addEventListener(
+  "input",
+  function () {
+    mealTotal(
+      "starterDishes2",
+      "mainDishes2",
+      "dessertDishes2",
+      "drinks2",
+      "total2"
+    );
+  },
+  false
+);
+idName("form3").addEventListener(
+  "input",
+  function () {
+    mealTotal(
+      "starterDishes3",
+      "mainDishes3",
+      "dessertDishes3",
+      "drinks3",
+      "total3"
+    );
+  },
+  false
+);
+idName("form4").addEventListener(
+  "input",
+  function () {
+    mealTotal(
+      "starterDishes4",
+      "mainDishes4",
+      "dessertDishes4",
+      "drinks4",
+      "total4"
+    );
+  },
+  false
+);
+idName("form5").addEventListener(
+  "input",
+  function () {
+    mealTotal(
+      "starterDishes5",
+      "mainDishes5",
+      "dessertDishes5",
+      "drinks5",
+      "total5"
+    );
+  },
+  false
+);
 
-  //function to add-up a single column, i.e, all starters, all drinks...
+//function to add-up a single column, i.e, all starters, all drinks...
 
-  function addItems(item1,item2,item3,item4,item5,totalDisplay){
-    let dish1= +idName(item1).value;
-    let dish2= +idName(item2).value;
-    let dish3= +idName(item3).value;
-    let dish4= +idName(item4).value;
-    let dish5= +idName(item5).value;
-    dishTotal= dish1+dish2+dish3+dish4+dish5;
-    idName(totalDisplay).value=dishTotal;
-    idName(totalDisplay).innerHTML =dishTotal + " EURO";
-  }
-  idName("menu").addEventListener("change",function(){
-    addItems("starterDishes1","starterDishes2","starterDishes3","starterDishes4","starterDishes5","startersTotal");
-  }, false);
-  idName("menu").addEventListener("change",function(){
-    addItems("mainDishes1","mainDishes2","mainDishes3","mainDishes4","mainDishes5","mainsTotal");
-  }, false);
-  idName("menu").addEventListener("change",function(){
-    addItems("dessertDishes1","dessertDishes2","dessertDishes3","dessertDishes4","dessertDishes5","dessertsTotal");
-  }, false); 
-  idName("menu").addEventListener("change",function(){
-    addItems("drinks1","drinks2","drinks3","drinks4","drinks5","drinksTotal");
-  }, false);
-  
+function addItems(item1, item2, item3, item4, item5, totalDisplay) {
+  let dish1 = +idName(item1).value;
+  let dish2 = +idName(item2).value;
+  let dish3 = +idName(item3).value;
+  let dish4 = +idName(item4).value;
+  let dish5 = +idName(item5).value;
+  dishTotal = dish1 + dish2 + dish3 + dish4 + dish5;
+  idName(totalDisplay).value = dishTotal;
+  idName(totalDisplay).innerHTML = dishTotal + " EURO";
+}
+idName("menu").addEventListener(
+  "change",
+  function () {
+    addItems(
+      "starterDishes1",
+      "starterDishes2",
+      "starterDishes3",
+      "starterDishes4",
+      "starterDishes5",
+      "startersTotal"
+    );
+  },
+  false
+);
+idName("menu").addEventListener(
+  "change",
+  function () {
+    addItems(
+      "mainDishes1",
+      "mainDishes2",
+      "mainDishes3",
+      "mainDishes4",
+      "mainDishes5",
+      "mainsTotal"
+    );
+  },
+  false
+);
+idName("menu").addEventListener(
+  "change",
+  function () {
+    addItems(
+      "dessertDishes1",
+      "dessertDishes2",
+      "dessertDishes3",
+      "dessertDishes4",
+      "dessertDishes5",
+      "dessertsTotal"
+    );
+  },
+  false
+);
+idName("menu").addEventListener(
+  "change",
+  function () {
+    addItems(
+      "drinks1",
+      "drinks2",
+      "drinks3",
+      "drinks4",
+      "drinks5",
+      "drinksTotal"
+    );
+  },
+  false
+);
 
-function totalFinal(){
-  total=+idName("startersTotal").value+idName("mainsTotal").value+idName("dessertsTotal").value+idName("drinksTotal").value;
-  idName("totalFinal").innerHTML = "€"+total+"EUR";
+function totalFinal() {
+  total =
+    +idName("startersTotal").value +
+    idName("mainsTotal").value +
+    idName("dessertsTotal").value +
+    idName("drinksTotal").value;
+  idName("totalFinal").innerHTML = "€" + total + "EUR";
 }
 
 //using my function totalFinal to update my total space, which shows all of the orders added up
 
 idName("menu").addEventListener("change", totalFinal);
 
-
-
 //function that fetches the API and brings it to the webpage
 idName("myBtn").addEventListener("click", getData);
 
 function getData() {
-
-
   //Get API
   fetch("https://randomuser.me/api/?results=5")
     .then((res) => res.json())
@@ -183,7 +287,3 @@ function getData() {
       idName("output").innerHTML = output;
     });
 }
-
-
-
-
--- js/scriptMenu1.js
+++ js/scriptMenu1.js
@@ -1,19 +1,31 @@
 $(document).ready(function () {
   $("#startersBtn").click(function () {
-    $("#myStarters").fadeToggle(500);
+    $("#myStarters").slideToggle(1000);
   });
 
   $("#mainsBtn").click(function () {
     $("#myMains").slideToggle(1000);
   });
   $("#dessertsBtn").click(function () {
-    $("#myDesserts").fadeToggle(500);
+    $("#myDesserts").slideToggle(1000);
   });
   $("#drinksBtn").click(function () {
     $("#myDrinks").slideToggle(1000);
   });
+  $(".moveAround").click(function () {
+    var box = $(this);
+    box.animate({
+      top: 100,
+    });
+    box.animate({
+      top: 400,
+    });
+    box.animate({
+      top: 100,
+    });
+  });
 
-  /*
+  /*  
   $("#pic").on({
     mouseenter: function () {
       $(this).css("background-image", "url(../img/background2.jpg)");
@@ -30,15 +42,41 @@ $(document).ready(function () {
   var currentPic = 0;
 
   function nextPicture() {
-    picture.css(
-      "background-image",
-      //when it reaches the last img of the array it goes back to the first one, so it's essentially a loop
-      backgrounds[(currentPic = ++currentPic % backgrounds.length)]
-    );
+    picture
+      .css(
+        "background-image",
+        //when it reaches the last img of the array it goes back to the first one, so it's essentially a loop
+        backgrounds[(currentPic = ++currentPic % backgrounds.length)]
+      )
+      .animate({ opacity: 1 }, { duration: 1000 });
 
     setTimeout(nextPicture, 2000);
   }
   //Here I'm calling the function nextPicure
   setTimeout(nextPicture, 2000);
   picture.css("background-image", backgrounds[0]);
+
+  $(".menuCard")
+    .on("mouseenter", function () {
+      var card = $(this);
+      card.stop(true, true).animate(
+        {
+          margin: -10,
+          width: "+=50",
+          height: "+=50",
+        },
+        "fast"
+      );
+    })
+    .on("mouseleave", function () {
+      var card = $(this);
+      card.stop(true, true).animate(
+        {
+          margin: +10,
+          width: "-=50",
+          height: "-=50",
+        },
+        "fast"
+      );
+    });
 });
